50c7996d63ed43295f70b6c52962feda5f1b7d16
Peter "PayteR" Gašparík <payter86@gmail.com>
2026-04-05 22:34:34 +0200

    refactor: remove support schema exports from core-support module

--- src/server/db/schema/support.ts
+++
@@ -1,3 +0,0 @@
-// Re-export all support tables from core-support module for schema discovery
-export { saasSupportChatSessions, saasSupportChatMessages } from '@/core-support/schema/support-chat';
-export { saasTickets, saasTicketMessages } from '@/core-support/schema/support-tickets';
